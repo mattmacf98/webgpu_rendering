1a6bf31ac64e1aeadf06c5948d46892266669495
mattmacf98 <macfarqu@amazon.com>
2024-11-20 16:37:29 -0500

    add surface normals

--- basics/src/App.tsx
+++ basics/src/App.tsx
@@ -77,8 +77,11 @@ const App = () => {
     // webGpuContext.instance!.render_depth_testing(depthTestingWgsl, 10, 1, positions, Float32Array.from(transformationMatrix), Float32Array.from(projectionMatrix), primitiveState, depthStencilState);
 
     //MODEL LOADING
-    const transformationMatrix = glMatrix.mat4.lookAt(glMatrix.mat4.create(), glMatrix.vec3.fromValues(3, 3, 3), glMatrix.vec3.fromValues(0, 0, 0), glMatrix.vec3.fromValues(0.0, 0.0, 1.0));
+    const modelViewMatrix = glMatrix.mat4.lookAt(glMatrix.mat4.create(), glMatrix.vec3.fromValues(3, 3, 3), glMatrix.vec3.fromValues(0, 0, 0), glMatrix.vec3.fromValues(0.0, 0.0, 1.0));
     const projectionMatrix = glMatrix.mat4.perspective(glMatrix.mat4.create(), 1.4, 640.0 / 480.0, 0.1, 1000.0);
+    const modelViewMatrixInverse = glMatrix.mat4.invert(glMatrix.mat4.create(), modelViewMatrix);
+    const normalMatrix = glMatrix.mat4.transpose(glMatrix.mat4.create(), modelViewMatrixInverse);
+
     const primitiveState: GPUPrimitiveState = {
       topology: 'triangle-list' as GPUPrimitiveTopology,
       frontFace: 'ccw' as GPUFrontFace,
@@ -89,7 +92,7 @@ const App = () => {
       depthCompare: 'less' as GPUCompareFunction,
       format: 'depth24plus-stencil8' as GPUTextureFormat,
     }
-    webGpuContext.instance!.render_obj_model(objModelWgsl, "teapot.obj", Float32Array.from(transformationMatrix), Float32Array.from(projectionMatrix), primitiveState, depthStencilState);
+    webGpuContext.instance!.render_obj_model(objModelWgsl, "teapot.obj", Float32Array.from(modelViewMatrix), Float32Array.from(projectionMatrix), Float32Array.from(normalMatrix), primitiveState, depthStencilState);
   }
 
   useEffect(() => {
@@ -453,7 +456,7 @@ class WebGPUContext {
     this._device.queue.submit([commandEncoder.finish()]);
   }
 
-  public async render_obj_model(shaderCode: string, objFilePath: string, transformationMatrix: Float32Array, projectionMatrix: Float32Array, primitiveState: GPUPrimitiveState, depthStencilState: GPUDepthStencilState) {
+  public async render_obj_model(shaderCode: string, objFilePath: string, transformationMatrix: Float32Array, projectionMatrix: Float32Array, normalMatrix: Float32Array, primitiveState: GPUPrimitiveState, depthStencilState: GPUDepthStencilState) {
     const objResponse = await fetch(objFilePath);
     const objBlob = await objResponse.blob();
     const objText = await objBlob.text();
@@ -463,6 +466,7 @@ class WebGPUContext {
 
     const transformationMatrixBuffer = this._createGPUBuffer(transformationMatrix, GPUBufferUsage.UNIFORM);
     const projectionMatrixBuffer = this._createGPUBuffer(projectionMatrix, GPUBufferUsage.UNIFORM);
+    const normalMatrixBuffer = this._createGPUBuffer(normalMatrix, GPUBufferUsage.UNIFORM);
 
     const transformationMatrixBindGroupInput: IBindGroupInput = {
       type: "buffer",
@@ -472,18 +476,24 @@ class WebGPUContext {
       type: "buffer",
       buffer: projectionMatrixBuffer,
     }
+    const normalMatrixBindGroupInput: IBindGroupInput = {
+      type: "buffer",
+      buffer: normalMatrixBuffer,
+    }
   
-    const { bindGroupLayout: uniformBindGroupLayout, bindGroup: uniformBindGroup } = this._createUniformBindGroup([transformationMatrixBindGroupInput, projectionMatrixBindGroupInput]);
+    const { bindGroupLayout: uniformBindGroupLayout, bindGroup: uniformBindGroup } = this._createUniformBindGroup([transformationMatrixBindGroupInput, projectionMatrixBindGroupInput, normalMatrixBindGroupInput]);
 
     const { buffer: positionBuffer, layout: positionBufferLayout } = this._createSingleAttributeVertexBuffer(objDataExtractor.vertexPositions, { format: "float32x3", offset: 0, shaderLocation: 0 }, 3 * Float32Array.BYTES_PER_ELEMENT);
+    const { buffer: normalBuffer, layout: normalBufferLayout } = this._createSingleAttributeVertexBuffer(objDataExtractor.normals, { format: "float32x3", offset: 0, shaderLocation: 1 }, 3 * Float32Array.BYTES_PER_ELEMENT);
     const indexBuffer = this._createGPUBuffer(objDataExtractor.indices, GPUBufferUsage.INDEX);
    
     const commandEncoder = this._device.createCommandEncoder();
 
     const passEncoder = commandEncoder.beginRenderPass(this._createRenderTarget(depthTexture));
     passEncoder.setViewport(0, 0, this._canvas.width, this._canvas.height, 0, 1);
-    passEncoder.setPipeline(this._createPipeline(this._createShaderModule(shaderCode), [positionBufferLayout], [uniformBindGroupLayout], primitiveState, depthStencilState));
+    passEncoder.setPipeline(this._createPipeline(this._createShaderModule(shaderCode), [positionBufferLayout, normalBufferLayout], [uniformBindGroupLayout], primitiveState, depthStencilState));
     passEncoder.setVertexBuffer(0, positionBuffer);
+    passEncoder.setVertexBuffer(1, normalBuffer);
     passEncoder.setIndexBuffer(indexBuffer, "uint16");
     passEncoder.setBindGroup(0, uniformBindGroup);
     passEncoder.drawIndexed(objDataExtractor.indices.length, 1, 0, 0, 0);
@@ -502,18 +512,38 @@ class WebGPUContext {
 class ObjDataExtractor {
   private _vertexPositions: Float32Array;
   private _indices: Uint16Array;
-
+  private _normals: Float32Array;
   constructor(objText: String) {
     const objFileParser = new ObjFileParser(objText);
     const objFile = objFileParser.parse();
     this._vertexPositions = new Float32Array(objFile.models[0].vertices.flatMap(v => [v.x, v.y, v.z]));
 
-    const indices = [];
+    const indices: number[] = [];
+    const normals: number[] = Array(this._vertexPositions.length).fill(0);
     for (const face of objFile.models[0].faces) {
+      let points = [];
+      let facet_indices = [];
       for (const v of face.vertices) {
-        indices.push(v.vertexIndex - 1);
+        const index = v.vertexIndex - 1;
+        indices.push(index);
+
+        const vertex = glMatrix.vec3.fromValues(this._vertexPositions[index * 3], this._vertexPositions[index * 3 + 1], this._vertexPositions[index * 3 + 2]);
+        points.push(vertex);
+        facet_indices.push(index);
+      }
+
+      const v1 = glMatrix.vec3.subtract(glMatrix.vec3.create(), points[1], points[0]);
+      const v2 = glMatrix.vec3.subtract(glMatrix.vec3.create(), points[2], points[0]);
+      const cross = glMatrix.vec3.cross(glMatrix.vec3.create(), v1, v2);
+      const normal = glMatrix.vec3.normalize(glMatrix.vec3.create(), cross);
+
+      for (let i  of facet_indices) {
+        normals[i*3] += normal[0];
+        normals[i*3 + 1] += normal[1];
+        normals[i*3 + 2] += normal[2];
       }
     }
+    this._normals = new Float32Array(normals);
 
     this._indices = new Uint16Array(indices);
   }
@@ -524,5 +554,9 @@ class ObjDataExtractor {
 
   public get indices(): Uint16Array {
     return this._indices;
+  }
+
+  public get normals(): Float32Array {
+    return this._normals;
   }
 }
